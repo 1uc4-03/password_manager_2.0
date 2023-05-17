16b9f741ce89b41a9eec1f47ef83f62f2d1c49fa
Luca Desktop <lucaj.t.156@gmail.com>
2023-05-17 02:37:36 +0200

    imgui inclusion

---
+++ src/app/display.cpp
@@ -0,0 +1,10 @@
+#include "display.h"
+#include "imgui.h"
+
+namespace Display {
+
+    void renderUI() {
+        ImGui::ShowDemoWindow();
+    }
+
+}
---
+++ src/app/display.h
@@ -0,0 +1,12 @@
+#ifndef PASSWORD_MANAGHER_2_DISPLAY_H
+#define PASSWORD_MANAGHER_2_DISPLAY_H
+
+
+namespace Display{
+
+    void renderUI();
+
+}
+
+
+#endif
