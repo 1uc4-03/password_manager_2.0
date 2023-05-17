df11e6072c51f1e18d6cd760d008c744f62ffccc
Luca Desktop <lucaj.t.156@gmail.com>
2023-05-17 13:14:48 +0200

    switch to x64

--- src/libFunctions/functions.cpp
+++ src/libFunctions/functions.cpp
@@ -1,11 +1,17 @@
 #include "functions.h"
-using std::string;
+#include "argon2.h"
 
-auto encrypt(string const & s) {
+#include <string>
+#include <map>
+
+// "This is the password manager. It securely stores your account data.\nManage your passwords by using the following commands:\n'n' stores a new password.\n'p' shows account info for a specific platform.\n'e' shows accounts connected to a specific email.\n'v' displays all listed accounts.\n'd' deletes a password by the corresponding platform.\n'c' cloes the manager.\n";
+
+
+auto encrypt(std::string const & s) {
 
 }
 
-auto decrypt(string const & s) {
+auto decrypt(std::string const & s) {
 
 }
 
--- src/libFunctions/functions.h
+++ src/libFunctions/functions.h
@@ -1,11 +1,10 @@
 #ifndef PASSWORD_MANAGHER_2_FUNCTIONS_H
 #define PASSWORD_MANAGHER_2_FUNCTIONS_H
 
+#pragma once
+
 #include <string>
 #include <map>
-#include <iostream>
-
-#include "argon2.h"
 
 auto encrypt(std::string const & s);
 auto decrypt(std::string const & s);
@@ -19,7 +18,6 @@ void viewAllAcc();
 
 typedef void (*FnPtr)();
 
-std::string disclaimerText = "This is the password manager. It securely stores your account data.\nManage your passwords by using the following commands:\n'n' stores a new password.\n'p' shows account info for a specific platform.\n'e' shows accounts connected to a specific email.\n'v' displays all listed accounts.\n'd' deletes a password by the corresponding platform.\n'c' cloes the manager.\n";
 std::map<char, FnPtr> commands = {
     {'n', &newAcc},
     {'p', &getAccByPassword},
